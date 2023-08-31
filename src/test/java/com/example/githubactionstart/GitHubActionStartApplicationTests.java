9810b01c6dc85923900c2e881bcd7163cba321ca
Najamoe <naja0718@stud.kea.dk>
2023-08-31 15:02:56 +0200

    initial commit

---
+++ src/test/java/com/example/githubactionstart/GitHubActionStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
